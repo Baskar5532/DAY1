db36ffa5e8815b5c61bf319fd720d1072f927029
Baskar5532 <st.baskar001@gmail.com>
2022-02-28 20:23:07 +0530

    new2

--- src/org/all/Languageclass.java
+++ src/org/all/Languageclass.java
@@ -5,6 +5,8 @@ import org.tamil.Tamil;
 public class Languageclass extends Tamil{
 	public void allLanguage() {
 		System.out.println("ALL LANGUAGE ");
+		System.out.println("nrrechvb");
+		
 	}
 public static void main(String[] args) {
 	Languageclass L =new Languageclass();
